dfab42ffb14b92ff3a8b6e3195490f9476bfdc43
David Wu <david204code@gmail.com>
2021-03-18 21:47:00 +0000

    Add submit EventListener with dummy condition and using reference function

--- app.js
+++ app.js
@@ -14,8 +14,29 @@ let editID = "";
 
 // ****** EVENT LISTENERS **********
 
+// submit forms using reference instead of an empty function
+form.addEventListener("submit", addItem);
+
 // ****** FUNCTIONS **********
 
 // ****** LOCAL STORAGE **********
+function addItem(e) {
+  e.preventDefault();
+  // access the value of the input
+  // console.log(grocery.value);
+  const value = grocery.value;
+  // short cut to get unique ID for item before adding to the list
+  const id = new Date().getTime().toString();
+
+  // (value && !editFlag)
+  if (value !== "" && editFlag === false) {
+    console.log("Adding values");
+    // (value && editFlag)
+  } else if (value !== "" && editFlag === true) {
+    console.log("Edit values");
+  } else {
+    console.log("empty value");
+  }
+}
 
 // ****** SETUP ITEMS **********
