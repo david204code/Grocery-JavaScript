336f4edd9e86d88774c456f9d12830d6d4ed9e0b
David Wu <david204code@gmail.com>
2021-03-18 21:55:50 +0000

    Add the condition when the submition is empty AND add displayAlert function with a setTimeout function to remove alert

--- app.js
+++ app.js
@@ -35,8 +35,20 @@ function addItem(e) {
   } else if (value !== "" && editFlag === true) {
     console.log("Edit values");
   } else {
-    console.log("empty value");
+    displayAlert("please enter value", "danger");
   }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // set time out funciton to remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
 
 // ****** SETUP ITEMS **********
